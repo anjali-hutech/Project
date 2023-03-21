206b6c38a89216db033921a352024789f43ccdf4
anjali-hutech <m.anjali@hutechsolutions.com>
2023-03-21 14:28:31 +0530

    ...

--- SpringbootExampleProjec/src/main/java/com/example/springboot/entity/Customer.java
+++ SpringbootExampleProjec/src/main/java/com/example/springboot/entity/Customer.java
@@ -23,7 +23,7 @@ public class Customer {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	
-	private String name;
+	private String names;
 	private BigDecimal balance;
 	private BigDecimal totalExpenses;
 	private BigDecimal totalEarnings;
